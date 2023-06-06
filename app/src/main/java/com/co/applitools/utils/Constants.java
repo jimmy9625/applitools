2bcd2242992db332ab9c5899393e401bfdda33b5
Jaime Velasquez <jaime@mentu.co>
2023-06-06 12:10:02 -0500

    pruebea front

---
+++ app/src/main/java/com/co/applitools/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.co.applitools.utils;
+
+public class Constants {
+
+
+    public static final String URL ="https://demo.applitools.com/hackathonV2.html";
+    public static final String  GREEN_COLOR="rgb(36, 179, 20)";
+    public static final String  RED_COLOR="rgb(230, 82, 82)";
+}
